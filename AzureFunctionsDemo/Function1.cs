3e6da5aef5dee980f0cc86a1294aae2bb3ca4a30
André Søvde <andre.sovde@capitalontap.com>
2019-04-12 16:08:10 +0100

    Updated logging

--- AzureFunctionsDemo/Function1.cs
+++ AzureFunctionsDemo/Function1.cs
@@ -22,6 +22,8 @@ namespace AzureFunctionsDemo
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
+            log.Info("Finished processing.");
+
             return name != null
                 ? (ActionResult)new OkObjectResult($"Hello, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
