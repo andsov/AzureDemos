8dd0896b611e6fa26cf03c727bbd0687c909b62a
André Søvde <andre.sovde@capitalontap.com>
2019-04-26 16:00:41 +0100

    Added new project for testing various functions + deployment project

---
+++ AzureFunctionsDemo/Processing/HeaderParser.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace AzureFunctionsDemo.Processing
+{
+    public class HeaderParser<T> : IHeaderParser<T>
+        where T : class, new()
+    {
+        public T Parse(IHeaderDictionary headerDictionary)
+        {
+            var t = new T();
+
+            t.GetType();
+
+            //foreach(var propName in t.)
+
+            return t;
+        }
+    }
+
+    public interface IHeaderParser<T>
+        where T : class, new()
+    {
+        T Parse(IHeaderDictionary headerDictionary);
+    }
+}
